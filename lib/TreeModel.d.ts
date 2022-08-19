3828df527c73f09c6c84c94383010bc834ea6d15
PinghuaZhuang <zphua2016@gmail.com>
2022-08-19 18:06:12 +0800

    update: 重新打包.

--- lib/TreeModel.d.ts
+++ lib/TreeModel.d.ts
@@ -1,68 +1,73 @@
 export interface Data {
-    id: string | number;
-    pId: string | number;
-    name: string;
-    childList: Data[];
-    className?: string;
-    level: number;
-    expand?: boolean;
+  id: string | number;
+  pId: string | number;
+  name: string;
+  childList: Data[];
+  className?: string;
+  level: number;
+  expand?: boolean;
 }
 declare type Map = {
-    [P: string]: TreeModel;
+  [P: string]: TreeModel;
 };
 export declare type Diff = {
-    [P: string]: {
-        indeterminate?: boolean;
-        checked?: boolean;
-    };
+  [P: string]: {
+    indeterminate?: boolean;
+    checked?: boolean;
+  };
 };
-declare type EachCallback = (data: TreeModel, parent: TreeModel, index: number) => void;
+declare type EachCallback = (
+  data: TreeModel,
+  parent: TreeModel,
+  index: number,
+) => void;
+declare type Ids = Data['id'][];
 declare class TreeModel implements Data {
-    #private;
-    id: Data['id'];
-    pId: Data['pId'];
-    level: Data['level'];
-    options: Data;
-    name: Data['name'];
-    childList: TreeModel[];
-    parent?: TreeModel;
-    className?: Data['className'];
-    map: Map;
-    constructor(options: Data, parent?: TreeModel);
-    isRoot(): boolean;
-    get checked(): boolean;
-    get indeterminate(): boolean;
-    diff(): {
-        indeterminate?: boolean | undefined;
-        checked?: boolean | undefined;
-    };
-    /**
-     * checked 中要使用 indeterminate. 赋值的时候注意顺序.
-     */
-    setChecked(value: boolean, unrecursion?: boolean): void;
-    setIndeterminate(value: boolean, unrecursion?: boolean): void;
-    /**
-     * 通过ID设置选中
-     */
-    setCheckedReturnDiff(id?: Data['id'], value?: boolean): Diff;
-    /**
-     * 根据ID数组设置选中, 返回Diff
-     */
-    selectKeys(keys: Data['id'][]): Diff;
-    /**
-     * 获取当前树选中的状态
-     */
-    getSelectKeys(): (string | number)[];
-    /**
-     * 计算父元素的状态
-     */
-    calcParentStatus(): void;
-    /**
-     * 清空当前树的状态并返回 diff
-     */
-    clean(): Diff;
-    each(fn: EachCallback): void;
-    eachDeep(fn: EachCallback): void;
-    eachDeepAfter(fn: EachCallback): void;
+  #private;
+  id: Data['id'];
+  pId: Data['pId'];
+  level: Data['level'];
+  options: Data;
+  name: Data['name'];
+  childList: TreeModel[];
+  parent?: TreeModel;
+  className?: Data['className'];
+  map: Map;
+  constructor(options: Data, parent?: TreeModel);
+  isRoot(): boolean;
+  get checked(): boolean;
+  get indeterminate(): boolean;
+  diff(): {
+    indeterminate?: boolean | undefined;
+    checked?: boolean | undefined;
+  };
+  /**
+   * checked 中要使用 indeterminate. 赋值的时候注意顺序.
+   */
+  setChecked(value: boolean, unrecursion?: boolean): this;
+  setIndeterminate(value: boolean, unrecursion?: boolean): this;
+  /**
+   * 通过ID设置选中
+   */
+  setCheckedReturnDiff(id?: Data['id'], value?: boolean): Diff;
+  /**
+   * 根据ID数组设置选中, 返回Diff
+   */
+  selectKeys(keys: Ids): Diff;
+  /**
+   * 获取当前树选中的状态
+   */
+  getSelectKeys(): Ids;
+  /**
+   * 计算父元素的状态
+   */
+  calcParentStatus(): this | undefined;
+  /**
+   * 清空当前树的状态并返回 diff
+   */
+  clean(): Diff;
+  each(fn: EachCallback): this;
+  eachDeep(fn: EachCallback): this;
+  eachDeepAfter(fn: EachCallback): this;
 }
 export default TreeModel;
