889e8bae3c56638e6dbb713d4e90fc41b865c850
PinghuaZhuang <zphua2016@gmail.com>
2022-08-19 16:49:51 +0800

    create project.

---
+++ README.md
@@ -0,0 +1,3 @@
+# TreeModel
+
+数据结构数据, 类似 `antd` 的 `Tree` 组件. `checked` 状态变化的数据模型. 返回一个 `Diff` 状态变化集合.
---
+++ vite.config.ts
@@ -0,0 +1,38 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+import typescript from '@rollup/plugin-typescript';
+
+function resolve(url: string) {
+  return path.resolve(__dirname, url);
+}
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: './',
+  envPrefix: 'APP_',
+  server: {
+    host: '0.0.0.0',
+  },
+  build: {
+    outDir: 'lib',
+    lib: {
+      entry: resolve('./src/TreeModel.ts'),
+      name: 'TreeModel',
+      formats: ['es', 'umd'],
+      fileName: (format) => `index.${format}.js`
+    },
+    rollupOptions: {
+      plugins: [
+        typescript({
+          target: 'es2015',
+          rootDir: resolve('./src'),
+          declaration: true,
+          declarationDir: resolve('./lib'),
+          exclude: resolve('node_modules/**'),
+          tsconfig: resolve('./tsconfig.json'),
+        }),
+      ]
+    },
+  },
+  plugins: [],
+});
